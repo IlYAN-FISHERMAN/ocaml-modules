43b7abb1e77fed54f5c33ea3b702cfda4c86566a
IlYAN-FISHERMAN <Ilkay.yanar71@proton.me>
2026-08-30 19:31:08 +0200

    donne

--- ml01/ex02/main.ml
+++ ml01/ex02/main.ml
@@ -1,1 +1,27 @@
-let () = ()
+let () =
+    print_int (Ackermann.ackermann 3 3);
+    print_newline();
+    print_int (Ackermann.ackermann 3 8);
+    print_newline();
+    print_int (Ackermann.ackermann 3 10);
+    print_newline();
+    print_int (Ackermann.ackermann 4 0);
+    print_newline();
+    print_int (Ackermann.ackermann 4 1);
+    print_newline();
+    print_int (Ackermann.ackermann (-1) 7);
+    print_newline();
+    (*
+    print_int (Ackermann.ackermann 4 2); c'est non
+    print_newline();
+    *)
+
+
+    (*
+        3 3     61              instantané
+        3 8     2045            ~1 s
+        3 10    8189            ~30 s
+        4 0     13              instantané
+        4 1     65533           des heures, ou stack overflow
+        4 2     2^65536 − 3     jamais
+    *)
